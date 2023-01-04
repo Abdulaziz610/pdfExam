2fd73c2ccb781ad8a0e952c281ee3c085bea96a3
Abdulaziz610 <abdulazizalnasseri1998@gmail.com>
2023-01-03 21:42:14 -0800

    codeUpdate

--- pdfExam.java
+++ pdfExam.java
@@ -21,33 +21,37 @@ public class pdfExam {
 	
 	public static void merge(File source, File dest) throws IOException {
 
-		// copy file in folder
+		// Function to copy files to folders
 		Files.copy(source.toPath(), dest.toPath());
 	}
-	
+	//Main
   public static void main(String args[]) throws IOException {
 	  
-	  
+	  // Loop to create 100 pdf files
     try {
     	for(int i = 0; i <= 100; i++) {
     		Document document = new Document();
     		OutputStream outputStream = new FileOutputStream(new File("C:\\Users\\Lenovo\\eclipse-workspace\\javaExam\\src\\javaExam\\TestFiles\\TestFile" + i + ".pdf"));
     		PdfWriter.getInstance(document, outputStream);
     		document.open();
+    		// Select the first 15 file and write into it
     		if(i <= 15) {
     			document.add(new Paragraph("Arabic"));
     			document.add(new Paragraph("English"));
     		}
+    		// Select 16 to 40 to add into it
     		else if(i > 16 && i <= 40) {
     			document.add(new Paragraph("Arabic"));
     			document.add(new Paragraph("English"));
     			document.add(new Paragraph("French"));
     			
     		}
+    		// Select 40 to 80 to add into it
     		else if(i > 40 && i <= 80) {
     			document.add(new Paragraph("Spain"));
     			document.add(new Paragraph("French"));
     		}
+    		// Select 80 to 100 to add into it
     		else {
     			document.add(new Paragraph("German"));
     			document.add(new Paragraph("English"));
@@ -57,28 +61,32 @@ public class pdfExam {
     		document.close();
     		outputStream.close();
     	}
-        System.out.println("Pdf Files has been created successfully.");
+        System.out.println(" ******* Files created successfully *******");
        } 
     	catch (Exception e) {
     	e.printStackTrace();
     }
-  //copy method
-
+  
+    // Copy and Search for a word
     Scanner sc = new Scanner(System.in);
     try {
     List<String> matchWords = new ArrayList<>();
     System.out.println("Enter a word to be searched?");
     String searchWord = sc.next();
+    // for loop to go through the 100 pdf file, and search for the word
     for (int counter = 0; counter <= 100; counter++) {
     PdfReader readFile = new PdfReader("C:\\Users\\Lenovo\\eclipse-workspace\\javaExam\\src\\javaExam\\TestFiles\\TestFile" + counter + ".pdf");
     String text = PdfTextExtractor.getTextFromPage(readFile, 1);
 
+    //Condition if the file has the searched word
     if (text.contains(searchWord)) {
     	matchWords.add("C:\\Users\\Lenovo\\eclipse-workspace\\javaExam\\src\\javaExam\\TestFiles\\TestFile" + counter + ".pdf");
     }
     	readFile.close();
     }
 
+    
+    //for each loop to go to all files and copy it to a copyFolder
     for (String matchFiles : matchWords) {
     	String fileName = matchFiles.substring(matchFiles.lastIndexOf("\\") + 1);
     	Document document = new Document();
